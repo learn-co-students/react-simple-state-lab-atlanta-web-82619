623b7832495dc5747461f22fb6cda858c23aaf09
Mark Del Mar <mark@1000martians.com>
2019-10-29 15:44:58 -0400

    Done.

---
+++ src/Cell.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class Cell extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      color: this.props.value
+    }
+  }
+
+  change = () => {
+    const newColor = '#333';
+    this.setState({
+      color: newColor
+    })
+  }
+
+  render() {
+    return (
+      <div className='cell' style={{ backgroundColor: this.state.color }} onClick={this.change}></div>
+    )
+  }
+}
+
+export default Cell;
